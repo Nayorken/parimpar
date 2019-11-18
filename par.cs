c24febb8a78fe32e250a7d24c0f8e04abb52c6c3
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-11-18 17:18:26 +0000

    Adicionar arquivos de projeto.

---
+++ par.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace parimpar
+{
+    class par
+    {
+        private double valor; 
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToDouble(value);   
+            }
+        }
+        public string envia
+        {
+            get
+            {
+                if (valor%2==1) return "O número " + valor + " é Impar.";
+                else return "O número " + valor + " é Par.";
+            }
+        }
+
+    }
+}
